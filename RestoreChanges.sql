8aa8e512fa2f24dad5e11f5b7946e6dc2a0c6a84
Wavesoft-BET <75376223+Wavesoft-BET@users.noreply.github.com>
2021-04-19 10:44:26 +0200

    Update RestoreChanges.sql

--- RestoreChanges.sql
+++ RestoreChanges.sql
@@ -4,7 +4,7 @@ WHERE clientID IN (
 		SELECT [ClientID]
 		FROM [EIS].[dbo].[Client]
 		WHERE FK_ClientStatusID = 1
-			AND FTPLocation LIKE '%10.3.2.69%'
+			AND FTPLocation LIKE '%10.3.2.191%'
 		)
 
 --System Settings
@@ -17,3 +17,25 @@ WHERE SystemSettingName = 'FileSource'
 -- [dbo].[XMLImport_Greyhound]
 -- [dbo].[XMLImport_eOddsMaker]
 -- [dbo].[ImportTXTInternationalFeeds] 
+
+DECLARE @SearchText varchar(1000) = '10.3.2.69';
+
+SELECT DISTINCT SPName 
+FROM (
+    (SELECT ROUTINE_NAME SPName
+        FROM INFORMATION_SCHEMA.ROUTINES 
+        WHERE ROUTINE_DEFINITION LIKE '%' + @SearchText + '%' 
+        AND ROUTINE_TYPE='PROCEDURE')
+    UNION ALL
+    (SELECT OBJECT_NAME(id) SPName
+        FROM SYSCOMMENTS 
+        WHERE [text] LIKE '%' + @SearchText + '%' 
+        AND OBJECTPROPERTY(id, 'IsProcedure') = 1 
+        GROUP BY OBJECT_NAME(id))
+    UNION ALL
+    (SELECT OBJECT_NAME(object_id) SPName
+        FROM sys.sql_modules
+        WHERE OBJECTPROPERTY(object_id, 'IsProcedure') = 1
+        AND definition LIKE '%' + @SearchText + '%')
+) AS T
+ORDER BY T.SPName
