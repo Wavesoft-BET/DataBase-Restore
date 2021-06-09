a7f9b15a20b99365e1a4293cc615230d78365c73
Wavesoft-BET <75376223+Wavesoft-BET@users.noreply.github.com>
2021-06-09 16:37:13 +0200

    Update RestoreChanges.sql

--- RestoreChanges.sql
+++ RestoreChanges.sql
@@ -5,14 +5,14 @@ SELECT [ClientID]
 FROM [EIS].[dbo].[Client]
 WHere FTPLocation LIKE '%10.3.2.191%'
 )
-
+--------------------------------------------------------------------------------------
 ---Stored proc locations need to be in System Settings
 --H:
 -- [dbo].[XMLImport_Greyhound]
 -- [dbo].[XMLImport_eOddsMaker]
 -- [dbo].[ImportTXTInternationalFeeds] 
 -- [dbo].[XMLImport_SISHorseRacing]
- 
+----------------------------------------------------------------------------------------- 
 UPDATE [EIS].[dbo].SystemSettings
 SET SystemSettingValue = '\\10.3.104.104\RaceFileOutput\\'
 WHERE SystemSettingName = 'FileSource'
@@ -45,8 +45,13 @@ UPDATE [EISImport].[dbo].SystemSettings
 SET SystemSettingValue = 'H:\Incoming\TSXMLOutputFiles'
 WHERE SystemSettingName = 'TurfsportOutputXML'  
 
+--------------------------------------------------------------------
+ ---Stored proc locations need to be in System Settings
+-- 10.3.2.69 -> 10.3.104.104
+-- 10.3.2.65 -> 10.3.104.103
+-- moz-syx-sql1 - >10.3.105.101
+---------------------------------------------------------------------
 
- 
 DECLARE @SearchText varchar(1000) = '10.3.2.69';
 
 SELECT DISTINCT SPName
@@ -58,6 +63,47 @@ UNION ALL
 (SELECT OBJECT_NAME(id) SPName
 FROM SYSCOMMENTS
 WHERE [text] LIKE '%' + @SearchText + '%'
+AND OBJECTPROPERTY(id, 'IsProcedure') = 1
+GROUP BY OBJECT_NAME(id))
+UNION ALL
+(SELECT OBJECT_NAME(object_id) SPName
+FROM sys.sql_modules
+WHERE OBJECTPROPERTY(object_id, 'IsProcedure') = 1
+AND definition LIKE '%' + @SearchText + '%')) AS T
+ORDER BY T.SPName
+
+
+DECLARE @SearchPush varchar(1000) = '10.3.2.65';
+
+SELECT DISTINCT SPName
+FROM ((SELECT ROUTINE_NAME SPName
+FROM INFORMATION_SCHEMA.ROUTINES
+WHERE ROUTINE_DEFINITION LIKE '%' + @SearchText + '%'
+AND ROUTINE_TYPE='PROCEDURE')
+UNION ALL
+(SELECT OBJECT_NAME(id) SPName
+FROM SYSCOMMENTS
+WHERE [text] LIKE '%' + @SearchPush + '%'
+AND OBJECTPROPERTY(id, 'IsProcedure') = 1
+GROUP BY OBJECT_NAME(id))
+UNION ALL
+(SELECT OBJECT_NAME(object_id) SPName
+FROM sys.sql_modules
+WHERE OBJECTPROPERTY(object_id, 'IsProcedure') = 1
+AND definition LIKE '%' + @SearchText + '%')) AS T
+ORDER BY T.SPName
+
+DECLARE @SearchMoz varchar(1000) = 'moz-syx-sql1';
+
+SELECT DISTINCT SPName
+FROM ((SELECT ROUTINE_NAME SPName
+FROM INFORMATION_SCHEMA.ROUTINES
+WHERE ROUTINE_DEFINITION LIKE '%' + @SearchText + '%'
+AND ROUTINE_TYPE='PROCEDURE')
+UNION ALL
+(SELECT OBJECT_NAME(id) SPName
+FROM SYSCOMMENTS
+WHERE [text] LIKE '%' + @SearchMoz + '%'
 AND OBJECTPROPERTY(id, 'IsProcedure') = 1
 GROUP BY OBJECT_NAME(id))
 UNION ALL
